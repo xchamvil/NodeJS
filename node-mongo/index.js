e8d92814e6b371fb671ffaea77e6ef685380603f
Xavier Cham Villa <xchamvil@Xaviers-MacBook-Pro.local>
2020-06-06 16:00:43 -0700

    mongoose example

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -9,25 +10,36 @@ MongoClient.connect(url, (err,client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name":"Uthapizza","description":"test"}, 
-    (err,result) => {
-        assert.equal(err,null);
+    
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+    "dishes")
+    .then((result) => {
+        console.log("Insert Document:\n", result.ops);
 
-        console.log("After insert \n");
-        console.log(result.ops);
+        return dboper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+        console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err,docs) => {
-            assert.equal(err,null);
-            console.log("Found \n");
-            console.log(docs);
+        return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes");
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+    })
+    .then((result) => {
+        console.log("Updated Document:\n", result.result);
 
-                client.close();
-            });
-        });
-    });
+        return dboper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+        console.log("Found Updated Documents:\n", docs);
+                        
+        return db.dropCollection("dishes");
+    })
+    .then((result) => {
+        console.log("Dropped Collection: ", result);
+
+        return client.close();
+    })
+    .catch((err) => console.log(err));
 
 });
